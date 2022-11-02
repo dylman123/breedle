52257dfbd546c05b92ffc1e74a894bbcbe630d89
dylman123 <dylanklein29@gmail.com>
2022-11-02 16:48:29 +1100

    created groupNames

---
+++ src/domain/groups.mapping.ts
@@ -0,0 +1,9 @@
+export const groupNames: Record<number, string> = {
+  1: "Group 1 (Toys)",
+  2: "Group 2 (Terriers)",
+  3: "Group 3 (Gundogs)",
+  4: "Group 4 (Hounds)",
+  5: "Group 5 (Working Dogs)",
+  6: "Group 6 (Utility)",
+  7: "Group 7 (Non Sporting)",
+};
