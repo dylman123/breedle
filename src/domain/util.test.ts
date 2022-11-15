2815702f401125b4190097c51ae2f73919cccd03
dylman123 <dylanklein29@gmail.com>
2022-11-15 12:37:09 +1100

    added more functional unit tests

--- src/domain/util.test.ts
+++ src/domain/util.test.ts
@@ -1,33 +1,81 @@
-import { getResultEmoji } from "./util";
+import { getResultEmoji, isNameCorrect, isGroupCorrect, isOriginCorrect, isSizeCorrect } from "./util";
 import { breeds } from './breeds.mapping';
-import puppeteer from "puppeteer";
+// import puppeteer from "puppeteer";
+
+const target = breeds.find((b) => b.code === "french_bulldog");
+const frenchBulldog = {
+    code: "french_bulldog",
+    name: "French Bulldog",
+    group: 7,
+    origin: ["FR"],
+    size: [1],
+    correct: true,
+};
+const poodle = {
+    code: "poodle",
+    name: "Poodle",
+    group: 7,
+    origin: ["FR", "DE"],
+    size: [1, 2],
+    correct: false,
+};
+const goldenRetriever = {
+    code: "golden_retriever",
+    name: "Golden Retriever",
+    group: 3,
+    origin: ["GB"],
+    size: [2],
+    correct: false,
+};
 
 test('should display correct emoji', () => {
-    const exampleTarget = breeds.find((b) => b.code === "french_bulldog");
-    const correctGuess = {
-        code: "french_bulldog",
-        name: "French Bulldog",
-        group: 7,
-        origin: ["FR"],
-        size: [1],
-        correct: true,
-    };
-    const incorrectGuess = {
-        code: "poodle",
-        name: "Poodle",
-        group: 7,
-        origin: ["FR", "DE"],
-        size: [1, 2],
-        correct: false,
-      };
-    
     // Incorrect test
-    const emojiIncorrect = getResultEmoji(incorrectGuess, exampleTarget);
+    const emojiIncorrect = getResultEmoji(poodle, target);
     expect(emojiIncorrect).toBe("❌");
 
     // Correct test
-    const emojiCorrect = getResultEmoji(correctGuess, exampleTarget);
+    const emojiCorrect = getResultEmoji(frenchBulldog, target);
     expect(emojiCorrect).toBe("✅");
+});
+
+test('should match breed name', () => {
+    // Incorrect test
+    const resultIncorrect = isNameCorrect(poodle, target);
+    expect(resultIncorrect).toBe(false);
+
+    // Correct test
+    const resultCorrect = isNameCorrect(frenchBulldog, target);
+    expect(resultCorrect).toBe(true);
+});
+
+test('should match breed group', () => {
+    // Incorrect test
+    const resultIncorrect = isGroupCorrect(goldenRetriever, target);
+    expect(resultIncorrect).toBe(false);
+
+    // Correct test
+    const resultCorrect = isGroupCorrect(poodle, target);
+    expect(resultCorrect).toBe(true);
+});
+
+test('should match breed origin', () => {
+    // Incorrect test
+    const resultIncorrect = isOriginCorrect(goldenRetriever.origin[0], target?.origin);
+    expect(resultIncorrect).toBe(false);
+
+    // Correct test
+    const resultCorrect = isOriginCorrect(poodle.origin[0], target?.origin);
+    expect(resultCorrect).toBe(true);
+});
+
+test('should match breed size', () => {
+    // Incorrect test
+    const resultIncorrect = isSizeCorrect(goldenRetriever.size[0], target?.size);
+    expect(resultIncorrect).toBe(false);
+
+    // Correct test
+    const resultCorrect = isOriginCorrect(poodle.size[0], target?.size);
+    expect(resultCorrect).toBe(true);
 });
 
 // The following code has a bug:
